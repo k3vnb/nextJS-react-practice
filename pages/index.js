b1c33a822d37b2899ea9ec1688257dfff4bad3f7
Kevin Boyle <papershack@gmail.com>
2019-02-21 18:02:08 -0800

    link to json, render data from json in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Layout from "../components/Layout";
-
+const questions = require('./../questions.json');
+const question = (questions) => {
+    let date = new Date();
+    let day = date.getDate();
+    console.log(day);
+}
 
 
 const Index = () => (
     <Layout title="Home">
-        <iframe height="400px" width="100%" src="https://repl.it/@lemurriot/SeriousPeruDoom-1?lite=true" scrolling="no" frameBorder="no" allowtransparency="true" allowfullscreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
+        <div>{JSON.stringify(questions.level1[0].task)}</div>
+        <div>See a solution: {JSON.stringify(questions.level1[0].solutionLink)}</div>
+        <iframe height="400px" width="100%" src="https://repl.it/@lemurriot/SeriousPeruDoom-1?lite=true" scrolling="no" frameBorder="no" allowtransparency="true" allowFullScreen="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>
     </Layout>
 );
 
